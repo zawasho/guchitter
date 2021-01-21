95561f4055ebdaee0ed8d780bbdae4dd3e5fa280
zawasho <shoozawa20@gmail.com>
2021-01-21 12:57:18 +0000

    [一言ランダム表示]

--- app/assets/javascripts/thanks.js
+++ app/assets/javascripts/thanks.js
@@ -1,3 +1,9 @@
-const messages = ['今日もお疲れ様でした', 'ゆっくりお休みください', '美味しいご飯を食べましょう', 'お風呂でのんびりしましょう'];
-  const messageNo = Math.floor(Math.random() * messages.length);
-  console.log(messages[messageNo]);
+// thanks.html.erbに記述
+// window.onload = function() {
+//   const messages = ['今日もお疲れ様でした', 'ゆっくりお休みください', '美味しいご飯を食べましょう', 'お風呂でのんびりしましょう'];
+//   const messageNo = Math.floor(Math.random() * messages.length);
+//   var thanks = document.getElementById('divthanks');
+//   if (thanks != null) {
+//     thanks.textContent = messages[messageNo];
+//   }
+// }
